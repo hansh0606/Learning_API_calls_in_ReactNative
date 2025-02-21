9912192cb77ce8ad97bf4a4dce636946fd6711af
“hansh0606” <hanshpatil1982@gmail.com>
2025-02-21 15:44:50 +0530

    [Updated] : To do List with Class Components

--- App.js
+++ App.js
@@ -3,16 +3,21 @@ import { View, Text, SafeAreaView, ScrollView } from 'react-native';
 import Advance_Hands_On_Exercise from './src/Tasks/Month1_Week4_Advance_Hands_On_Exercise/Advance_Exercise';
 import Counter_App from './src/Tasks/Month2_Week1_Task1/Counter_App';
 import To_do_App from './src/Tasks/Month2_Week1_Task2/To_do_App'
+import TodoList from './src/Tasks/Month2_Week1_Task2/To_do_with_Class_Components';
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: '#fff' }}>
-      <ScrollView>
+      
         
-        <To_do_App/>
-        {/* <Counter_App/> */}
-        {/* <Advance_Hands_On_Exercise /> */}
-      </ScrollView>
+        {/* <To_do_App/> */}
+        <TodoList/>
+        {/* <Counter_App/> 
+        <ScrollView >
+
+        <Advance_Hands_On_Exercise />
+        </ScrollView> */}
+      
     </View>
   );
 }
